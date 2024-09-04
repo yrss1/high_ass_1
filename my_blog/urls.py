f237c7d52a3dcd0c494a86eaf3be4b701db98f31
yerassylrymkul <rymkulerasyl@gmail.com>
2024-09-04 12:07:08 +0500

    add auth

--- my_blog/urls.py
+++ my_blog/urls.py
@@ -16,11 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('auth/', include('account.urls')),
     path('blogs/', include('blog.urls')),
-    # path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
-    # path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
