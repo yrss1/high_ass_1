be062d67a06212b57d1932413c46ed9d8c576d2f
yerassylrymkul <rymkulerasyl@gmail.com>
2024-09-04 10:33:34 +0500

    exercise-3

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
